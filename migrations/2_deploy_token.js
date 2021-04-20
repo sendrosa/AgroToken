05e3277d7b91b8b2373b11d56345c31a6754193f
andrsend <andrsend@ee.duth.gr>
2021-04-20 14:24:02 +0300

    agro test

---
+++ migrations/2_deploy_token.js
@@ -0,0 +1,7 @@
+const agrotoken = artifacts.require("AgroToken");
+
+module.exports = function (deployer) {
+    const _name= "AgroToken";
+    const _symbol="AGT";
+    deployer.deploy(agrotoken, _name, _symbol);
+};
