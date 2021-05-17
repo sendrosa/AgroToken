e32a53b0ce0ed255361ef31c61c309a25ca7e743
sendrosa <sendrosaster@gmail.com>
2021-05-17 09:40:46 -0400

    buy token

--- contracts/AgroToken.sol
+++ contracts/AgroToken.sol
@@ -3,9 +3,11 @@ pragma solidity >=0.4.22 <0.9.0;
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract AgroToken is ERC20{
-    constructor(string memory _name, string memory _symbol) 
+    constructor(string memory _name, string memory _symbol)
         ERC20(_name, _symbol)
     {
 
     }
+
+
 }
