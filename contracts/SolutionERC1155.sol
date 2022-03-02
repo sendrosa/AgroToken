4c2a57d38d63d539c8eff12333c90bd48dd88546
andrsend <sendrosaster@gmail.com>
2022-03-02 12:16:10 +0200

    add id in 1155 and transfer

--- contracts/SolutionERC1155.sol
+++ contracts/SolutionERC1155.sol
@@ -4,14 +4,27 @@ import "openzeppelin-solidity/contracts/token/ERC1155/ERC1155.sol";
 
 contract SolutionERC1155 is ERC1155{
 
-    uint public constant soultanina=0;
-    uint public constant korinthiaki=1;
+    event tokeninfo(uint indexed id, uint token_id, address owner);
+    uint nonce=0;
 
     constructor() ERC1155("example.json"){}
 
     //just mine new token
     function mint(uint amount,uint token_id) public{
         _mint(msg.sender,token_id, amount,"");
+    }
+
+    function mintnew(uint amount,uint token_id) public{
+        _mint(msg.sender,token_id, amount,"");
+        for (uint i = 0; i < amount; i++) {
+            emit tokeninfo(nonce, token_id, msg.sender);
+            nonce++;
+        }
+    }
+
+    function transfer(uint id,uint token_id,address to) public{
+        _safeTransferFrom(msg.sender,to,token_id,1,"");
+        emit tokeninfo(id, token_id, to);
     }
 
     function mintbatch(uint[] memory amount,uint[] memory tokens_id) public{
--- test/SolutionERC1155.test.js
+++ test/SolutionERC1155.test.js
@@ -20,6 +20,29 @@ contract("solution", accounts => {
             console.log(balance1-balance2);
         });
     });
+
+    describe("mintnew supply ERC1155", function(){
+        it("create correct supply for an id", async function(){
+            let balance1 = await web3.eth.getBalance(accounts[0]);
+            await this.token.mintnew(15, 0);
+            const total= (await this.token.balanceOf("0x884e3a4912DAd1BfE07844F835f463cd9fD046A4",0)).toString();
+            total.should.equal('15');
+            let balance2 = await web3.eth.getBalance(accounts[0]);
+            console.log(balance1-balance2);
+        });
+    });
+
+    describe("transfer ERC1155", function(){
+        it("create correct supply and transfer it", async function(){
+            await this.token.mintnew(15, 0);
+            await this.token.transfer(2,0,accounts[1]);
+            const total= (await this.token.balanceOf(accounts[1],0)).toString();
+            total.should.equal('1');
+            console.log(total);
+        });
+    });
+    
+
     describe("mint batch supply ERC1155", function(){
         it("create correct supply for an id", async function(){
             let balance1 = await web3.eth.getBalance(accounts[0]);
